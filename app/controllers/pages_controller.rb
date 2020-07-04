92af63f7435fb50acb32523eec15a980a691eb77
Viktor Araujo <vkaraujo00@gmail.com>
2020-07-04 02:47:19 -0300

    js script written

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,30 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :authenticate_user!, except: %i[home highscores]
+  before_action :set_stats, only: %i[game]
 
   def home
   end
+
+  def game
+  end
+
+  def highscores
+    @users = User.order("wins DESC")
+  end
+
+  def played
+    current_user.wins += 1 if params[:win]
+    current_user.losses += 1 if params[:loss]
+    current_user.ties += 1 if params[:tie] 
+    current_user.save   
+  end  
+
+  private
+
+  def set_stats
+    @wins = current_user.wins
+    @losses = current_user.losses
+    @ties = current_user.ties
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'game', to: 'pages#game'
+  get 'highscores', to: 'pages#highscores'
+  post '/played', to: 'pages#played'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
